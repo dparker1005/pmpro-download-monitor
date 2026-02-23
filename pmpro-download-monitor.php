2b44a4338907c606818993b1ef72001ce46531ee
David Parker <dlparker1005@gmail.com>
2026-02-23 14:28:41 -0500

    Links and docs

--- pmpro-download-monitor.php
+++ pmpro-download-monitor.php
@@ -1,11 +1,15 @@
 <?php
-/*
-Plugin Name: Paid Memberships Pro - Download Monitor Integration Add On
-Plugin URI: http://www.paidmembershipspro.com/pmpro-download-monitor/
-Description: Require membership for downloads when using the Download Monitor plugin.
-Version: .2.1
-Author: Stranger Studios
-Author URI: http://www.strangerstudios.com
+/**
+ * Plugin Name: Paid Memberships Pro - Download Monitor Integration Add On
+ * Plugin URI: https://www.paidmembershipspro.com/add-ons/pmpro-download-monitor/
+ * Description: Require membership for downloads when using the Download Monitor plugin.
+ * Version: .2.1
+ * Author: Paid Memberships Pro
+ * Author URI: https://www.paidmembershipspro.com
+ * Text Domain: pmpro-download-monitor
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
 
 /*
  * Add Require Membership box to dlm_download CPT
@@ -228,8 +232,8 @@ function pmprodlm_plugin_row_meta($links, $file) {
 	if(strpos($file, 'pmpro-download-monitor.php') !== false)
 	{
 		$new_links = array(
-			'<a href="' . esc_url('http://www.paidmembershipspro.com/add-ons/plus-add-ons/pmpro-download-monitor/')  . '" title="' . esc_attr( __( 'View Documentation', 'pmpro' ) ) . '">' . __( 'Docs', 'pmpro' ) . '</a>',
-			'<a href="' . esc_url('http://paidmembershipspro.com/support/') . '" title="' . esc_attr( __( 'Visit Customer Support Forum', 'pmpro' ) ) . '">' . __( 'Support', 'pmpro' ) . '</a>',
+			'<a href="' . esc_url('https://www.paidmembershipspro.com/add-ons/pmpro-download-monitor/')  . '" title="' . esc_attr( __( 'View Documentation', 'pmpro' ) ) . '">' . __( 'Docs', 'pmpro' ) . '</a>',
+			'<a href="' . esc_url('https://www.paidmembershipspro.com/support/') . '" title="' . esc_attr( __( 'Visit Customer Support Forum', 'pmpro' ) ) . '">' . __( 'Support', 'pmpro' ) . '</a>',
 		);
 		$links = array_merge($links, $new_links);
 	}
